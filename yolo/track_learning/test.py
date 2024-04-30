660efe28a120ca79009b94c29a132bfcecb1d68f
luoxiaojun <xiao-jun.luo@hp.com>
2024-04-30 14:17:16 +0800

    feat: detect whether person is approaching

--- yolo/track_learning/test.py
+++ yolo/track_learning/test.py
@@ -6,9 +6,9 @@
 # pip install ultralytics
 # pip install lapx
 
-import cv2
+import torch, cv2
 from ultralytics import YOLO
-import torch
+from ultralytics.solutions import speed_estimation, distance_calculation
 
 print(torch.cuda.is_available())
 
@@ -18,6 +18,18 @@ model = YOLO('yolov8n.pt')
 # Open the video file
 cap = cv2.VideoCapture(0)
 
+# Init speed-estimation obj
+line_pts = [[0, 360], [1280, 360]]
+speed_obj = speed_estimation.SpeedEstimator()
+speed_obj.set_args(reg_pts=line_pts, names=model.model.names)
+
+# Init dist obj
+dist_obj = distance_calculation.DistanceCalculation()
+dist_obj.set_args(names=model.model.names)
+
+boxesSizes = {}
+sizeChangeThreshold = 30000
+
 # Loop through the video frames
 while cap.isOpened():
     # Read a frame from the video
@@ -25,18 +37,33 @@ while cap.isOpened():
 
     if success:
         # Run YOLOv8 tracking on the frame, persisting tracks between frames
-        results = model.track(frame, persist=True, device="1")
+        tracks = model.track(frame, persist=True, verbose=False, device="1")
 
         # Visualize the results on the frame
-        annotated_frame = results[0].plot()
-        for result in results:
-            boxes = result.boxes.cpu().numpy()
-            for label in boxes.cls:
+        annotated_frame = tracks[0].plot()
+        for track in tracks:
+            boxes = track.boxes.cpu().numpy()
+            for i, label in enumerate(boxes.cls):
                 if label == 0:
-                    print("person")
+                    if (boxes.xywh is not None) and (boxes.id is not None) and (i < len(boxes.xywh)) and (i < len(boxes.id)):
+                        size = boxes.xywh[i][2] * boxes.xywh[i][3]
+                        id = boxes.id[i]
+                        if id in boxesSizes:
+                            if size - boxesSizes[id] > sizeChangeThreshold:
+                                print("Alert! One person is approaching!")
+                                print(size)
+                                print(boxesSizes[id])
+                        boxesSizes[id] = size
+
+        frame_with_speed = speed_obj.estimate_speed(frame, tracks)
+
+        frame_with_dist = dist_obj.start_process(frame, tracks)
 
         # Display the annotated frame
-        cv2.imshow("YOLOv8 Tracking", annotated_frame)
+        # cv2.imshow("YOLOv8 Tracking", annotated_frame)
+        # cv2.imshow("YOLOv8 Tracking with Dist", frame_with_dist)
+        if frame_with_speed.size > 0:
+            cv2.imshow("YOLOv8 Tracking with Speed", frame_with_speed)
 
         # Break the loop if 'q' is pressed
         if cv2.waitKey(1) & 0xFF == ord("q"):
